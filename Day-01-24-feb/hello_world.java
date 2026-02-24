df097ba0c85d5957b822e8a5ca5c937a440f84a1
karthikeyan <karthikeyangopal16@gmail.com>
2026-02-24 14:01:59 +0100

    test

---
+++ Day-01-24-feb/hello_world.java
@@ -0,0 +1,18 @@
+public class hello_world {
+public static void main (String[] args) {
+
+    int i = 1;
+    int sum = 1 ;
+    int n = 6;
+
+    for(i=1;i<=n;i++) {
+        sum = i+sum ;
+        System.out.println(sum);
+    }
+    while(i<n)
+    {
+        System.out.println(i);
+        i++;
+    }
+    
+}}
