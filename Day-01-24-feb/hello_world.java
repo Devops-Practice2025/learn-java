fefe63ed5c611d4397beea4f5e45cfa9c1aa5f24
karthikeyan <karthikeyangopal16@gmail.com>
2026-02-24 16:52:44 +0100

    test

--- Day-01-24-feb/hello_world.java
+++ Day-01-24-feb/hello_world.java
@@ -3,16 +3,18 @@ public static void main (String[] args) {
 
     int i = 1;
     int sum = 1 ;
-    int n = 6;
+    int n = 5;
 
     for(i=1;i<=n;i++) {
         sum = i+sum ;
-        System.out.println(sum);
+        System.out.println("this is the sum inside for"+" "+sum);
     }
-    while(i<n)
-    {
-        System.out.println(i);
-        i++;
+    do {
+        i=i+1;
+        System.out.println("This is i inside do loop"+" "+i);
     }
-    
-}}
+
+    while(i<n) ;
+
+}
+}
