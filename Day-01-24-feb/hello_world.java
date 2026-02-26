369a5b1c46838bf8c77b46aba2d01a9d99fc954b
karthikeyan <karthikeyangopal16@gmail.com>
2026-02-26 08:01:23 +0100

    test

--- Day-01-24-feb/hello_world.java
+++ Day-01-24-feb/hello_world.java
@@ -1,24 +1,18 @@
-import java.util.Date;
-
 public class hello_world {
 public static void main (String[] args) {
 
     int i = 1;
     int sum = 1 ;
-    int n = 5;
-
-    Date today = new Date();
+    int n = 6;
 
     for(i=1;i<=n;i++) {
         sum = i+sum ;
-        System.out.println("this is the sum inside for"+" "+sum+" "+today);
+        System.out.println(value of sum+sum);
     }
-    do {
-        i=i+1;
-        System.out.println("This is i inside do loop"+" "+i);
+    while(i<n)
+    {
+        System.out.println(i);
+        i++;
     }
-
-    while(i<n) ;
-
-}
-}
+    
+}}
